23101b29a9846c946e0f163a871d76cdcae1190e
jwt388 <jtnh388@gmail.com>
2024-12-10 20:04:09 -0500

    Add motor types to config

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -19,7 +19,7 @@ import swervelib.math.Matter;
 public final class Constants
 {
 
-  public static final double ROBOT_MASS = 40.0 * 0.453592; // 32lbs * kg per pound
+  public static final double ROBOT_MASS = 40.0 * 0.453592; // 40lbs * kg per pound
   public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
   public static final double LOOP_TIME  = 0.13; //s, 20ms + 110ms sprk max velocity lag
   // Speed for Neo Vortex at 6700 RPM, 6.75:1 gears and 4" wheels
