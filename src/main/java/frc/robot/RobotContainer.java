3f19dffab8438aec051138b84ee850a82945ae05
jwt388 <jtnh388@gmail.com>
2024-12-30 11:54:12 -0500

    Use ToggleOnTrue to switch modes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -251,7 +251,7 @@ public class RobotContainer
       driverXbox.start().whileTrue(Commands.none());
       driverXbox.back().whileTrue(Commands.none());
       driverXbox.leftBumper().whileTrue(Commands.runOnce(drivebase::lock, drivebase).repeatedly());
-      driverXbox.rightBumper().onTrue(Commands.none());
+      driverXbox.rightBumper().toggleOnTrue(driveRobotOrientedAngularVelocity);
     }
 
   }
